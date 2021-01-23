643fc8af957d7b79ab78b5b1b7a429e98c0faa8d
jchicano <jesus.d.chicano@gmail.com>
2021-01-23 10:49:04 +0100

    Genereate getters and setters

--- src/main/java/io/github/jchicano/model/Account.java
+++ src/main/java/io/github/jchicano/model/Account.java
@@ -15,4 +15,36 @@ public class Account {
     private Currency currency;
     private int balance;
     private boolean treasury;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Currency getCurrency() {
+		return currency;
+	}
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public boolean isTreasury() {
+		return treasury;
+	}
+	public void setTreasury(boolean treasury) {
+		this.treasury = treasury;
+	}
+    
+    
 }
