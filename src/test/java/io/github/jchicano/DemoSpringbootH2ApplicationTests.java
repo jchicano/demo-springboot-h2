fff2aafb60b857e157464fcd8a1faa99a7345c0d
jchicano <jesus.d.chicano@gmail.com>
2021-01-23 10:37:14 +0100

    Initial commit

---
+++ src/test/java/io/github/jchicano/DemoSpringbootH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jchicano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
